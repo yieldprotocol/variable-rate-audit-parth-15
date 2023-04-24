e47a1ad5ffb641cdf19a287fed89787a391cce2d
iamsahu <praffulsahu@gmail.com>
2023-04-24 11:40:19 +0200

    fix: removal of debt addition for borrowing base against base

--- src/oracles/VariableInterestRateOracle.sol
+++ src/oracles/VariableInterestRateOracle.sol
@@ -179,10 +179,6 @@ contract VariableInterestRateOracle is IOracle, AccessControl, Constants {
             // Calculate the total debt
             uint128 totalDebt;
             DataTypes.Debt memory debt_;
-            debt_ = cauldron.debt(base.b6(), base.b6());
-            // Adding the debt borrowing base against itself
-            totalDebt = totalDebt + debt_.sum;
-
             // Adding the debt borrowing base against other ilks
             for (uint256 i = 0; i < rateParameters.ilks.length; i++) {
                 if (cauldron.ilks(base.b6(), rateParameters.ilks[i])) {
